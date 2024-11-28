c0090fb1f1031c48e8ee841b9be392bca9f334cd
aeijpe <aniek.eijpe1@gmail.com>
2024-11-28 14:42:43 +0100

    update gitignore

--- run_drit_test.sh
+++ run_drit_test.sh
@@ -5,7 +5,7 @@
 #SBATCH --gpus=1                                    # Number of gpus in total
 #SBATCH --ntasks=1                                  # Run on a single node
 #SBATCH --cpus-per-task=4                           # Number of cores
-#SBATCH --time=01:00:00                             # Time limit hrs:min:sec
+#SBATCH --time=02:00:00                             # Time limit hrs:min:sec
 #SBATCH --output=/projects/multimodal_disentanglement_review/DRIT/jobs/slurm_%j.log   
 # Standard output and error log
 pwd; hostname; date
@@ -28,8 +28,8 @@ cd src
 rsync -avv --info=progress2 --delete /projects/multimodal_disentanglement_review/nnUNet/nnUNet_preprocessed/Dataset033_MMWHSCT $SCRATCH/data_dir1
 rsync -avv --info=progress2 --delete /projects/multimodal_disentanglement_review/nnUNet/nnUNet_preprocessed/Dataset034_MMWHSMRI $SCRATCH/data_dir2
 # Train
-python train.py --name mmwhs_run_fold_0 --batch_size 188 --data_dir1 $SCRATCH/data_dir1/Dataset033_MMWHSCT --data_dir2 $SCRATCH/data_dir2/Dataset034_MMWHSMRI --cases_folds 0 --data_type nnUNet --n_ep 1000 --splits_dir1 ../splits/splits_mmwhs_ct.json --splits_dir2 ../splits/splits_mmwhs_mri.json
+# python train.py --name mmwhs_run_fold_0 --batch_size 188 --data_dir1 $SCRATCH/data_dir1/Dataset033_MMWHSCT --data_dir2 $SCRATCH/data_dir2/Dataset034_MMWHSMRI --cases_folds 0 --data_type nnUNet --n_ep 1000 --splits_dir1 ../splits/splits_mmwhs_ct.json --splits_dir2 ../splits/splits_mmwhs_mri.json
 
-# python evaluate_model.py --name mmwhs_run_fold_0 --data_type nnUNet --cases_fold 0 --data_dir1 $SCRATCH/data_dir1/Dataset_033_MMWHSCT/ --data_dir2 $SCRATCH/data_dir2/Dataset_034_MMWHSMRI/
+python evaluate_model.py --name small_mmwhs_run_fold_0 --data_type nnUNet --cases_fold 0 --data_dir1 $SCRATCH/data_dir1/Dataset_033_MMWHSCT/ --data_dir2 $SCRATCH/data_dir2/Dataset_034_MMWHSMRI/
 # python create_fake_img.py --resume results/mmwhs_run_fold_0/.pth --name mmwhs_run_fold_0 --a2b 0 --data_dir1 $SCRATCH/data_dir2/Dataset034_MMWHSMRI/ --cases_folds 0 --result_dir ../synthetic_data/Dataset041_MMWHSFAKECT/ --name_new_ds MMWHSFAKECT --data_type nnUNet
 # python create_fake_img.py --resume results/mmwhs_run_fold_0/.pth --name mmwhs_run_fold_0 --a2b 1 --data_dir1 $SCRATCH/data_dir1/Dataset033_MMWHSCT/ --cases_folds 0 --result_dir ../synthetic_data/Dataset042_MMWHSFAKEMRI/ --name_new_ds MMWHSFAKEMRI --data_type nnUNet
